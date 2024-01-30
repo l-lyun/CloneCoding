5994cccdde727a47858a097259bf0b5fd9a48ac3
l-lyun <kimdohyun032@gmail.com>
2024-01-30 18:48:28 +0900

    class component

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,8 +6,26 @@ import "./Board.css"
 
 export class Board extends Component {
   
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
   renderSqauare(i) { // 직접 board-row에 넣을 수 있지만, 공통되는 영역
-    return <Square />
+    // react에게 이 element 만들라고 지시
+    // square 컴포넌트 인스턴스 생성
+    // react 내부에서 자동적으로 render() 호출
+    return <Square value={this.state.squares[i]}
+    onClick={() => this.handleClick(i)}
+    />
+
   }
 
   render() {
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,19 @@
+// Square.js
+
 import React, { Component } from 'react'
 import "./Square.css"
 
 export default class Square extends Component {
+
+
+
   render() {
     return (
-      <button className="sqaure"> 
-        Square
+      <button className="sqaure" 
+      // react에서 JSX에서 JS문법을 사용할 때 {}로 묶어서 삽입한다
+      onClick={() => { this.props.onClick()}}> 
+      {/* 중괄호 주의하자! */}
+          {this.props.value}
       </button>
     )
   }
