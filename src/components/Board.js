462f1422b57fd9d956f9c42616a166faa6a404da
l-lyun <kimdohyun032@gmail.com>
2024-01-31 16:22:51 +0900

    fin

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,53 +1,53 @@
 // Board.js
 
-import React, {Component} from 'react'
 import Square from './Square'
 import "./Board.css"
 
-export class Board extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null)
-    }
-  }
+const Board = ({squares, onClick}) => {
+  // App.js에서 구현
+  // const handleClick = i => {
+  //   const newSquares = squares.slice();
+  //   if(calculateWinner(newSquares) || newSquares[i]) {
+  //     return;
+  //   }
+  //   newSquares[i] = xIsNext ? 'X' : 'O';
+  //   setSquares(newSquares);
+  //   // setXIsNext(!xIsNext);
+  //   // 위 아래가 여기서는 동일하다, 어떨 때 다를지 생각해보자!
+  //   setXIsNext(previousState => !previousState);
+  // }
 
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
-  }
-  renderSqauare(i) { // 직접 board-row에 넣을 수 있지만, 공통되는 영역
+
+
+  const renderSqauare = (i) => { // 직접 board-row에 넣을 수 있지만, 공통되는 영역
     // react에게 이 element 만들라고 지시
     // square 컴포넌트 인스턴스 생성
     // react 내부에서 자동적으로 render() 호출
-    return <Square value={this.state.squares[i]}
-    onClick={() => this.handleClick(i)}
+    return <Square value={squares[i]}
+    onClick={() => onClick(i)}
     />
 
   }
 
-  render() {
+
     return(
-      <div>
-        <div className='status'>Next Player: X, O</div>
+      <div className='board-wrapper'>
         <div className='board-row'>
-          {this.renderSqauare(0)}
-          {this.renderSqauare(1)}
-          {this.renderSqauare(2)}
+          {renderSqauare(0)}
+          {renderSqauare(1)}
+          {renderSqauare(2)}
         </div>
         <div className='board-row'>
-          {this.renderSqauare(3)}
-          {this.renderSqauare(4)}
-          {this.renderSqauare(5)}
+          {renderSqauare(3)}
+          {renderSqauare(4)}
+          {renderSqauare(5)}
         </div>
         <div className='board-row'>
-          {this.renderSqauare(6)}
-          {this.renderSqauare(7)}
-          {this.renderSqauare(8)}
+          {renderSqauare(6)}
+          {renderSqauare(7)}
+          {renderSqauare(8)}
         </div>
       </div>
     )
-  }
 }
+export default Board
--- src/components/Square.js
+++ src/components/Square.js
@@ -3,18 +3,15 @@
 import React, { Component } from 'react'
 import "./Square.css"
 
-export default class Square extends Component {
-
-
-
-  render() {
+const Square = ({onClick, value}) => {
     return (
       <button className="sqaure" 
       // react에서 JSX에서 JS문법을 사용할 때 {}로 묶어서 삽입한다
-      onClick={() => { this.props.onClick()}}> 
+      onClick={onClick}> 
       {/* 중괄호 주의하자! */}
-          {this.props.value}
+          {value}
       </button>
     )
-  }
 }
+
+export default Square
